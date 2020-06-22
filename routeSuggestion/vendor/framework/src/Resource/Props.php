d3ad2cbec4377a1954481c35c31453b3bc60f20e
Jeffery Edick <jeff@jasoncases.com>
2020-06-22 11:06:43 -0400

    Edit comments on Props, mostly getting comments under 72 chars

--- routeSuggestion/vendor/framework/src/Resource/Props.php
+++ routeSuggestion/vendor/framework/src/Resource/Props.php
@@ -3,9 +3,16 @@
 namespace Resource;
 
 /**
- * Props class takes an array and sets the keys to top-level properties of the
- * Props class. Props is then accessed in the controller from $this->props, and
- * then values are directly accessed as properties, rather than array syntax.
+ * Props class takes an array and sets the keys to top-level properties 
+ * of the Props class. Props is then accessed in the controller from 
+ * $this->props, and  * then values are directly accessed as properties,
+ * rather than array syntax.
+ * 
+ * Considering recursively calling Props all the way down, or possibly, 
+ * just on captured JSON strings
+ * 
+ * Since all data gets placed into the Props class, we can validate all
+ * user input in one place
  */
 class Props
 {
@@ -16,8 +23,8 @@ class Props
 
     /**
      * Set incoming array keys as top level props for the prop object
-     * goal is to access them without using array syntax, i.e., props->var
-     * rather than props['var']
+     * goal is to access them without using array syntax, i.e., 
+     * props->var, rather than props['var']
      *
      * @param array $props - array of incoming props
      *
@@ -25,6 +32,8 @@ class Props
      */
     private function _init($props): void
     {
+        // loop through each key/value pair and set the key as a 
+        // top-level property.
         foreach ($props as $key => $val) {
             $this->{$key} = $val;
         }
