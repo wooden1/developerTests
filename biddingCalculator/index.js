626ef7c5c49b01cc50057fa54522fe40b75dfd32
wooden1 <lynnwoodray2@protonmail.com>
2020-12-09 14:35:50 -0500

    got some fixme's and todo's, using better naming convention, added qseudo api call code

--- biddingCalculator/index.js
+++ biddingCalculator/index.js
@@ -1,41 +1,109 @@
-const bid_calculator_form = (function () {
-  'use strict'
+// TODO: create btn to fill total cost of goods sold
+const BiddingCalculator = (function () {
+  ;('use strict')
   const bid_info = []
-  function getBid() {
-    document
-      .querySelector('.bid_submit_btn')
-      .addEventListener('click', (event) => {
-        event.preventDefault()
-        // collect data from form fields
-        const elements = document.querySelectorAll('input')
-        // map over nodelist to return list of input elements
-        return bid_info.map.call(elements, (el) => el.value)
-      })
-  }
-  function addRow() {
-    document.querySelector('.add-row').addEventListener('click', (event) => {
-      const newRow = document.createElement('div')
-      newRow.className = 'costs-of-goods'
-      newRow.innerHTML = `<label for="cost-of-goods">Cost of goods sold:</label><input type="text" id="cost-of-goods"
-  name="cost-of-goods"></input>`
-      const newField = document.querySelector('.form-container-one')
-      event.preventDefault()
-      return newField.appendChild(newRow)
-    })
-  }
-  addRow()
+  const addRowButton = document.querySelector('.add-row')
+  // grabbed this one to copy the innerHTML to clean up that long str
+  const costOFGoodsRow = document.querySelector('#cost-of-goods-sold')
+  const totalOverhead = document.querySelector('#total-overhead')
+  const totalCostOFGoods = document.querySelector('#total-cost-of-goods-sold')
+  const profit = document.querySelector('#profit')
+  const total = document.querySelector('#total')
+  const sumOverheadBtn = document.querySelector('#get-overhead-sum')
+  const contratorInfoForm = document.querySelector('#contractor-info_form')
 
-  function sumOFTotals() {
-    const totalOverhead = document.querySelector('#total-overhead').value
-    const goodsSoldTotal = document.querySelector('#total-cost-of-goods-sold')
-      .value
-    const profit = document.querySelector('#profit').value
-    const total = document.querySelector('#total')
-    const sum =
-      parseInt(totalOverhead) + parseInt(goodsSoldTotal) + parseInt(profit)
-    document.querySelector('#get-total').addEventListener('click', () => {
-      return (total.value = sum)
+  function eventListeners() {
+    addRowButton.addEventListener('click', (event) => {
+      // event.preventDefault()
+      addRow(event)
+    })
+    sumOverheadBtn.addEventListener('click', (event) => {
+      // event.preventDefault()
+      sumOFTotals(event)
     })
   }
-  sumOFTotals()
+
+  // FIXME: get add row working again
+  function addRow() {
+    const newRow = document.createElement('div')
+    newRow.classList.add('cost-for-goods-sold')
+    newRow.innerHTML = costOFGoodsRow.innerHTML
+    return contratorInfoForm.appendChild(newRow)
+  }
+  // TODO: get values from cost for goods to fill total cost
+  function sumOfGoodsSold() {
+    return (totalCostOFGoods.value = parseFloat(costForGoodsRow))
+  }
+  function sumOFTotals() {
+    return (total.value =
+      parseFloat(totalOverhead) +
+      parseFloat(sumOfGoodsSold()) +
+      parseFloat(profit))
+  }
+
+  eventListeners()
+
+  // pseudo code for connecting to backend api
+
+  const url = 'http://backend-api.com/contract-bid_calculator-data.json'
+  const bidForm = document.querySelector('.bid-form')
+  const BidCalculatorPostData = {
+    contractorName: name,
+    contractorEmail: email,
+    contractorIndustry: industry,
+    contractorCostForGoodsSold: costForGoodsSold,
+    contractorTotalOverhead: overhead,
+    contractorTotalCostForGoodsSold: TotalCostForGoodsSold,
+    contractorProfit: profit,
+    contracorOverheadSum: overheadSum,
+  }
+  // send data to backend
+  const postReq = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(BidCalculatorPostData),
+  }
+
+  function listener(event) {}
+  // ping server
+  function status(response) {
+    if (response.status >= 200 && response.status < 300) {
+      return Promise.resolve(response)
+    }
+    return Promise.reject(new Error(response.statusText))
+  }
+
+  // checks that response status is greenlit
+  function checkStatus(response) {
+    if (response.ok) {
+      return Promise.resolve(response)
+    }
+    return Promise.reject(new Error(response.statusText))
+  }
+
+  // takes a url and checks for ok status, then returns json data
+  function fetchData(url) {
+    return fetch(url)
+      .then(checkStatus)
+      .then((res) => res.json())
+  }
+  function doSomethingwithThis() {
+    console.log('do something with data')
+  }
+  // post data to tbe backend
+  fetch(url, postReq)
+    .then((response) => {
+      if (!status(response)) {
+        console.log('bad request')
+      }
+      return response.json()
+    })
+    .then((data) => {
+      doSomethingwithThis(data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 })()
